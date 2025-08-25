7252d803bf2e4043f63ef398c01af0cefa7c6e75
hazracodegit <dudekulahazrabee@gmail.com>
2025-08-25 23:09:49 +0530

    Create InvertedRightAngledTrianglePattern.java

---
+++ InvertedRightAngledTrianglePattern.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class InvertedRightAngledTrianglePattern {
+   public static void main(String[] args){
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      for(int i=0;i<n;i++){
+         for(int j=1;j<=n-i;j++){
+            System.out.print("* ");
+         }
+        System.out.println();
+      }
+   }
+}
+
+/*  Output :
+    n=5
+
+      * * * * *
+      * * * *
+      * * *
+      * *
+      *
+
+      */
