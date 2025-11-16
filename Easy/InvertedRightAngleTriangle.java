facda86ab30e479c13771e95b5d2a1f8234e8223
hazracodegit <dudekulahazrabee@gmail.com>
2025-11-16 13:40:37 +0530

    Create InvertedRightAngleTriangle.java

---
+++ Easy/InvertedRightAngleTriangle.java
@@ -0,0 +1,22 @@
+// Inverted Right Angled Pattern
+/*    n=5
+      *  *  *  *  *
+      *  *  *  *
+      *  *  *
+      *  *
+      *
+
+  */
+import java.util.*;
+public class InvertedRightAngleTriangle{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    for(int i=1;i<=n;i++){
+      for(int j=1;j<=n-i+1;j++){
+        System.out.print("* );
+      }
+      System.out.println();
+    }
+  }
+}
